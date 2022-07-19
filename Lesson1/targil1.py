ed7852a2b606e218d17d9ce2930574ac6f3030cf
LEP5 <elipetrov77@gmail.com>
2022-07-19 20:01:38 +0300

    var type practice

--- Lesson1/targil1.py
+++ Lesson1/targil1.py
@@ -1,7 +1,11 @@
 name="Elijah"
 age=99
 print("hello " + name + "\nHow are you?\nYourageis: " + str(age))
-
+name = input("enter your name: ")
 mail = input("enter your mail: ")
+age = input("enter your age: ")
+
 print(mail)
 print(type(mail))
+
+print("Hello " + name + "/nYour age is: " + str(age) + "/nYour mail: " + mail)
